6dfc3562f139fcaee004c682dd932c3eb397ccd5
88Chandan <chandansahu257@gmail.com>
2026-09-01 22:20:44 +0530

    portfolio.logo

---
+++ src/components/Header.jsx
@@ -0,0 +1,48 @@
+import React,{ useState } from 'react'
+
+const Header = () => {
+    const [isMenuOpen,setIsMenuOpen] = useState(false);
+
+    const menuItems = [
+        {name:'Education',href:'#education'},
+        {name:'Certificates',href:'#Certificates'},
+        {name:'About Me',href:'#About'},
+        {name:'Skills',href:'#Skills'},
+        {name:'Projects',href:'#Projects'}
+
+    ]
+
+    const scrollToSection = (href) => {
+        setIsMenuOpen(false);
+        document.querySelector(href)?.scrollIntoView({
+            behavior: 'smooth'
+        })
+    }
+
+  return (
+    <header className='relative z-50 px-6 py-7'>
+        <div className='max-w-7xl mx-auto flex justify-between items-center'>
+            {/*Logo*/}
+            <div className='text-white text-3xl font-black cursor-pointer'>
+                Portfolio <span className='text-primary'>.</span>
+            </div>
+            {/* Navigation */}
+            <nav className='hidden md:flex items-center gap-10'>
+                <ul className='flex gap-8'>
+                    {menuItems.map((item) => {
+                        <li key={item.name}>
+                            <button 
+                            onClick={() => scrollToSection(item.href)}
+                            className='text-gray-300 hover:text-#b9c206 
+                            text-base font-medium transition-colors'
+                            >{item.name}</button>
+                        </li>
+                    })}
+                </ul>
+            </nav>
+        </div>
+    </header>
+  )
+}
+
+export default Header
