9dfa2e2c083d74d9347e5d3a1e145fb636453f03
88Chandan <chandansahu257@gmail.com>
2026-09-08 06:48:05 +0530

    start-projectsection

---
+++ src/components/Projects.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Projects = () => {
+    const projects = [
+        {
+            id:1,
+            image:proj1,
+            title:' portfolio',
+            dec:'collection of academic,skills, learning journey ',
+            tech:['React','Tailwind-css']
+        },
+              {
+            id:2,
+            image:proj2,
+            title:'RIFD Card Attendance',
+            dec:'collection of attendance by scanning rfid card',
+            tech:['Esp32','C++ programming','Google Spreadsheet']
+        },
+    ]
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Projects
